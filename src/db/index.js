e69ee025608ded9171d3d47a2c6c249b38a00d3f
dheerajbaheti <145109215+DurgaBaheti@users.noreply.github.com>
2024-05-18 16:55:40 +0000

    env varial=ble import

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
+const {MONGODB_URL} = process.env;
 
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/dheeraj`);
+        console.log(MONGODB_URL);
+        const connectionInstance = await mongoose.connect(MONGODB_URL);
         // const connectionInstance = await mongoose.connect(`mongodb+srv://dheeraj:dheeraj123@cluster0.hrrq7ua.mongodb.net/dheeraj`);
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
