2c3575e02a97b0c418363c1583ecca6292bea0ca
dheerajbaheti <145109215+DurgaBaheti@users.noreply.github.com>
2024-05-30 10:58:02 +0000

    tokens, userMethods and user login controller done

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,5 +1,29 @@
-import { ApiError, ApiResponse, asyncHandler, uploadOnCloudinary } from "../utils/index.js";
+import {
+  ApiError,
+  ApiResponse,
+  asyncHandler,
+  uploadOnCloudinary,
+} from "../utils/index.js";
 import { User } from "../models/user.models.js";
+import { access } from "fs";
+
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = User.generateAccessToken();
+    const refreshToken = User.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "something went wrong while generating access and refresh token !!"
+    );
+  }
+};
 
 const registerUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
@@ -14,7 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
   if (isExistedUser) {
     throw new ApiError(409, "user with email or username already exists");
   }
-  
+
   const avatarLocalPath = req.file?.path; // ----------------req.file does not contain path---error point
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar is required field");
@@ -33,18 +57,70 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 
   const createdUser = await User.findById(user._id).select(
-    "-resetPasswordToken -resetPasswordExpire"
+    "-password -resetPasswordToken -resetPasswordExpire"
   );
   if (!createdUser) {
-    throw new ApiError(400, "something went wrong while registering the user !!");
+    throw new ApiError(
+      400,
+      "something went wrong while registering the user !!"
+    );
   }
 
-  return res.status(201).json(
-    new ApiResponse(200, createdUser, "User registered Successfully")
-  )
-
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered Successfully"));
 });
 
+const loginUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username && !email) {
+    throw new ApiError(400, "USERNAME AND EMAIL IS REQUIRED");
+  }
 
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+  console.log("using $or ---", user);
+  if (!user) {
+    throw new ApiError(400, "userr does not exists !!");
+  }
 
-export { registerUser };
+  const isPaswordValid = await user.isPasswordCorrect(password);
+  if (!isPasswordValid) {
+    throw new ApiError(404, "PASSWORD is wrong !!");
+  }
+
+  const { accessToken, refreshToken } = generateAccessAndRefreshTokens(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("refreshToken", refreshToken, options)
+    .cookie("accessToken", accessToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "user logged in successfully"
+      )
+    );
+});
+
+export { 
+  registerUser,
+  loginUser,
+ };
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,53 +1,82 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import validator from "validator";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
-    {
-        username: {
-            type: String,
-            required: [true, "please enter your Name"],
-            maxLength:[30, "Name cannot exceed 30 characters"],
-            minLength:[4, "Name should have more than 4 characters"],
-            unique: true,
-            trim: true,
-            lowercase: true,
-        },
-        email: {
-            type: String,
-            required: [true, "please enter your Email"],
-            validate: [validator.isEmail, "please enter a valid Email !!"]
-        },
-        password: {
-            type: String,
-            required: [true, "please enter your Password"],
-            minLength:[8, "Name should have more than 8 characters"],
-            select: false
-        },
-        avatar: {
-            type: String, //cloudinary
-            // required: true,
-        },
-        role: {
-            type: String,
-            default: "user"
-        },
-        resetPasswordToken: String,
-        resetPasswordExpire: Date
-        
+  {
+    username: {
+      type: String,
+      required: [true, "please enter your Name"],
+      maxLength: [30, "Name cannot exceed 30 characters"],
+      minLength: [4, "Name should have more than 4 characters"],
+      unique: true,
+      trim: true,
+      lowercase: true,
     },
-    { timestamps: true })
+    email: {
+      type: String,
+      required: [true, "please enter your Email"],
+      validate: [validator.isEmail, "please enter a valid Email !!"],
+    },
+    password: {
+      type: String,
+      required: [true, "please enter your Password"],
+      minLength: [8, "Name should have more than 8 characters"],
+      select: false,
+    },
+    avatar: {
+      type: String, //cloudinary
+      // required: true,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    
+    refreshToken: String,
 
-    
-    userSchema.pre("save", async function (next) {
-        if(!this.isModified("password")) next();
-        
-        this.password = await bcrypt.hash(this.password, 10);
-        next();
-    })
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+  },
+  { timestamps: true }
+);
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) next();
 
-    
-    
-    
-    export const User = new mongoose.model("User", userSchema);
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = new mongoose.model("User", userSchema);
